c0f88adf9f06d85e762a172db266470a32662326
Diego Restrepo <restrepo@udea.edu.co>
2021-03-16 22:15:27 -0500

    script to update PDF

--- create_release.sh
+++ create_release.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+#Check last tag and creates new release
 yy=False
 zz=False
 x=$( git tag | tail -n1 | awk -F"." '{print $(NF)}')
 y=$( git tag | tail -n1 | awk -F"." '{print $(NF-1)}')
 z=$( git tag | tail -n1 | awk -F"." '{print $(NF-2)}' | awk -F'v' '{print $NF}')
 
+#Check x
 if [ $x -lt 9 ];then
     x=$(($x+1))
 else
@@ -28,8 +30,6 @@ fi
 if [ "$zz" ==  True ];then
     z=$(($z+1))
 fi
-
-
 
 echo git tag v${z}.${y}.${x}
 git tag v${z}.${y}.${x}
